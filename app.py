e491785ebe6824167cd30b580ec93ac13f5e0615
joewadk <jawadk7435@gmail.com>
2024-09-12 16:09:31 -0400

    using smtp lib now

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-import urllib.request
-import urllib.parse
+import smtplib
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
 import os
 load_dotenv()
-apikey= os.getenv('apiKey')
-def sendSMS(apikey, numbers, sender, message):
-    data =  urllib.parse.urlencode({'apikey': apikey, 'numbers': numbers, 
-                                    'message' : message, 'sender': 'Kanye'})
-    data = data.encode('utf-8')
-    request = urllib.request.Request("https://api.txtlocal.com/send/?")
-    f = urllib.request.urlopen(request, data)
-    fr = f.read()
-    return(fr)
- 
-resp =  sendSMS(apikey, '3475171648', 'Kanye', 'This is your message')
-print (resp)
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587 #specific port, most email clients use this one
+sender_email = os.getenv('email')
+sender_password = os.getenv('password')
+
+recipient_email = '3475171648@tmomail.net' #note this suffix is only for tmobile
+sms_body = 'Hello, this is a test SMS via email!'
+
+msg = MIMEText(sms_body)
+msg['From'] = sender_email
+msg['To'] = recipient_email
+msg['Subject'] = 'Test SMS'
+try:
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, recipient_email, msg.as_string())
+        print("SMS sent!")
+except:
+    print('you stupid')
